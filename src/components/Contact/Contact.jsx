132849f2b26ff886cc8d5749a16212d644636894
xandramiyo <aj.sumiyoshi@gmail.com>
2023-03-14 17:21:14 -0700

    mild styling to contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,19 +1,30 @@
 import './Contact.css'
+import linkedin from '../../images/linkedin.png'
+import github from '../../images/github.png'
 
 export default function Contact() {
 	return (
 		<section id="contact" className="flex col center">
 			<h3>Get in touch</h3>
-			<p>Whether you have an opportunity for me or just want to have a chat, fill out the form below and I'll get back to you as soon as I can!</p>
-			<form className="flex col">
-				<label>Name</label>
-				<input></input>
-				<label>Email</label>
-				<input></input>
-				<label>Message</label>
-				<textarea></textarea>
-				<button>send</button>
-			</form>
+			<div className="flex">
+				<div className="left-contact">
+					<p>Whether you have an opportunity for me or just want to have a chat, fill out the form below and I'll get back to you as soon as I can!</p>
+					<div className="contact-links">
+						<a href="">
+							<img src={linkedin} className="project-link"/>
+						</a>
+						<a href=""> 
+							<img src={github} className="project-link"/>
+						</a>
+					</div>
+				</div>
+				<form className="flex col right-contact">
+					<input placeholder="Name"></input>
+					<input placeholder="Email"></input>
+					<textarea placeholder="Message"></textarea>
+					<button>send</button>
+				</form>
+			</div>
 		</section>
 	)
 }
